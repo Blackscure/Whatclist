108e16b45aa30ba5e47ec0f6b37c099c084f39a0
blackscure <Wekesabuyahi@gmail.com>
2020-08-12 15:36:03 +0300

    wtf created

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app import app
 from .request import get_movies
 from .requests import get_movies,get_movie
+from flask import render_template,request,redirect,url_for
+
 
 
 # Views
@@ -17,7 +19,13 @@ def index():
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+    
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
      
 
 @app.route('/movie/<movie_id>')
